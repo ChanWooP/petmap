0684453ac603639b5b2e55e67071a6ab2e862b90
ChanWooP <cksdn4002@gmail.com>
2023-11-06 20:27:04 +0900

    테스트 코드 제거

--- src/test/java/com/cwpark/petmap/petmap/PetmapApplicationTests.java
+++ src/test/java/com/cwpark/petmap/petmap/PetmapApplicationTests.java
@@ -3,7 +3,7 @@ package com.cwpark.petmap.petmap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PetmapApplicationTests {
 
 	@Test
